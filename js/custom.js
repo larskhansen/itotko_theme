6a4e99b401630c97db3401f4350a224108daf95a
Lars Krogsgård Hansen <lkh@dbc.dk>
2025-05-17 09:16:05 +0200

    Boot strap 5 sub theme

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.itotko_theme = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
